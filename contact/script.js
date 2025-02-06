eed2cfa01e6288cf2fa9000b5ea587d5d6548a31
voting-age <razamohammad0589@gmail.com>
2025-02-07 00:55:26 +0530

    files added

---
+++ contact/script.js
@@ -0,0 +1,9 @@
+let menuContainer = document.querySelector('.menu-nav')
+let memuIcon = document.querySelector('.menu-icon')
+
+function openMenu(){
+    menuContainer.style.width = '200px';
+}
+function closeMenu(){
+    menuContainer.style.width = '0px'
+}
